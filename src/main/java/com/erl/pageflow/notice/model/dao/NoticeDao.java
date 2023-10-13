48389723e893f50a8079432c3e74561158b0ce86
sungyoung <choisy7120@naver.com>
2023-10-13 18:00:06 +0900

    2023-10-13 18:00 notice 기본구조 업데이트

---
+++ src/main/java/com/erl/pageflow/notice/model/dao/NoticeDao.java
@@ -0,0 +1,5 @@
+package com.erl.pageflow.notice.model.dao;
+
+public class NoticeDao {
+
+}
---
+++ src/main/java/com/erl/pageflow/notice/model/service/NoticeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.erl.pageflow.notice.model.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service("noticeService")
+public class NoticeServiceImpl implements NoticeService{
+	
+	
+
+}
