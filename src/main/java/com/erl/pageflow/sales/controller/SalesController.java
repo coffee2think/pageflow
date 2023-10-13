1ebcb6fd81730a789546300c4f2be5263b173530
coffee2think <mathematicy@gmail.com>
2023-10-13 17:08:36 +0900

    231013 17:08 sales 기본 구조

--- src/main/java/com/erl/pageflow/sales/controller/SalesController.java
+++ src/main/java/com/erl/pageflow/sales/controller/SalesController.java
@@ -1,5 +1,71 @@
 package com.erl.pageflow.sales.controller;
 
-public class SalesController {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.erl.pageflow.board.controller.BoardController;
+import com.erl.pageflow.book.model.vo.Book;
+import com.erl.pageflow.common.Search;
+import com.erl.pageflow.sales.model.service.SalesService;
+import com.erl.pageflow.sales.model.vo.BookOrder;
+import com.erl.pageflow.sales.model.vo.BookStore;
+
+@Controller
+public class SalesController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@Autowired
+	private SalesService salesService;
+	
+	//**************뷰페이지 이동****************
+	
+	// 주문현황 페이지 이동
+	@RequestMapping("moveboList.do")
+//	@RequestMapping("border_list.do")
+	public String moveBookOrderList(Model model) {
+		
+		// 첫 이동 시 해당 월의 데이터 조회
+		LocalDate today = LocalDate.now();
+		model.addAttribute("begin", today.minusDays(today.getDayOfMonth() - 1)); // 이번달 1일부터
+		model.addAttribute("end", today); // 오늘까지
+		
+		return "redirect:bolistDate.do";
+	}
+	
+	// 주문등록 페이지 이동
+	@RequestMapping("moveboInput.do")
+	public String moveBookOrderInputPage() {
+		return "sales/border_input";
+	}
+	
+	//**************요청 받아서 서비스로 넘기는 메소드****************
+	// 날짜로 주문현황 검색
+	@RequestMapping("bolistDate.do")
+	public String bookOrderList(Search search, Model model) {
+		
+		ArrayList<BookOrder> list = salesService.selectBookOrderDate(search);
+		for(BookOrder bo : list) {
+			Book book = salesService.selectBook(bo.getBookId());
+			BookStore bookStore = salesService.selectBookStore(bo.getClientId());
+			
+			bo.setBookName(book.getBookName());
+			bo.setBookPrice(book.getBookPrice());
+			bo.calcTotalPrice();
+			bo.setBookStoreName(bookStore.getClientName());
+		}
+
+		model.addAttribute("list", list);
+		model.addAttribute("begin", search.getBegin().toString());
+		model.addAttribute("end", search.getEnd().toString());
+		
+		return "sales/border_list";
+	}
 }
--- src/main/java/com/erl/pageflow/sales/model/vo/BookOrder.java
+++ src/main/java/com/erl/pageflow/sales/model/vo/BookOrder.java
@@ -7,20 +7,70 @@ public class BookOrder extends Trade {
 	
 	private static final long serialVersionUID = -6450254897030418120L;
 
+	private String bookName;
+	private int bookPrice;
+	private int totalPrice;
+	private String bookStoreName;
+	
 	public BookOrder() {
-		super();
 	}
 	
 	public BookOrder(int tradeId, int bookId, int clientId, int empId, String empName, int orderQuantity, Date orderDate,
 			String state, String classify) {
 		super(tradeId, bookId, clientId, empId, empName, orderQuantity, orderDate, state, classify);
 	}
+	
+	public BookOrder(int tradeId, int bookId, int clientId, int empId, String empName, int orderQuantity, Date orderDate,
+			String state, String classify, String bookName, int bookPrice, int totalPrice, String bookStoreName) {
+		super(tradeId, bookId, clientId, empId, empName, orderQuantity, orderDate, state, classify);
+		this.bookName = bookName;
+		this.bookPrice = bookPrice;
+		this.totalPrice = totalPrice;
+		this.bookStoreName = bookStoreName;
+	}
+	
+	public void calcTotalPrice() {
+		totalPrice = this.getBookPrice() * this.getOrderQuantity();
+	}
+	
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public int getBookPrice() {
+		return bookPrice;
+	}
+
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public String getBookStoreName() {
+		return bookStoreName;
+	}
+
+	public void setBookStoreName(String bookStoreName) {
+		this.bookStoreName = bookStoreName;
+	}
 
 	@Override
 	public String toString() {
 		return "BookOrder [tradeId=" + super.getTradeId() + ", bookId=" + super.getBookId() + ", clientId=" + super.getClientId() + ", empId=" + super.getEmpId()
 				+ ", empName=" + super.getEmpName() + ", orderQuantity=" + super.getOrderQuantity() + ", orderDate=" + super.getOrderDate() + ", state="
-				+ super.getState() + ", classify=" + super.getClassify() + "]";
+				+ super.getState() + ", classify=" + super.getClassify() + ", bookName=" + this.bookName + ", bookPrice=" + this.bookPrice
+				+ ", totalPrice=" + this.totalPrice + ", bookStoreName=" + this.bookStoreName + "]";
 	}
 	
 	
