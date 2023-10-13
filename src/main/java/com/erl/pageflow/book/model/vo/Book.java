6a4cb6d1b4d6b48aa25dc8eaf24f996dbd07cc7c
jeougun <skyjo95@gmail.com>
2023-10-13 14:09:01 +0900

    패키지 생성

---
+++ src/main/java/com/erl/pageflow/book/model/vo/Book.java
@@ -0,0 +1,8 @@
+package com.erl.pageflow.book.model.vo;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+	private static final long serialVersionUID = -6417677392956824886L;
+
+}
---
+++ src/main/java/com/erl/pageflow/writer/model/vo/Writer.java
@@ -0,0 +1,8 @@
+package com.erl.pageflow.writer.model.vo;
+
+import java.io.Serializable;
+
+public class Writer implements Serializable {
+	private static final long serialVersionUID = 2472594043505031700L;
+
+}
