438cf6426fb67a872cbc82e00762032cfcf97830
jeougun <skyjo95@gmail.com>
2023-10-16 14:04:52 +0900

    1016오전

--- src/main/java/com/erl/pageflow/book/model/service/BookService.java
+++ src/main/java/com/erl/pageflow/book/model/service/BookService.java
@@ -1,5 +1,29 @@
 package com.erl.pageflow.book.model.service;
 
-public interface BookService {
+import java.util.ArrayList;
 
+import com.erl.pageflow.book.model.vo.Book;
+import com.erl.pageflow.common.Paging;
+
+public interface BookService {
+	// 도서 리스트 개수 조회
+	public int selectBookListCount();
+	
+	// 도서 리스트 조회
+	public ArrayList<Book> selectBookList(Paging paging);
+	
+	// 도서 필터링한 리스트 조회
+	public ArrayList<Book> selectBookSearch(Paging paging);
+	
+	// 도서 조회
+	public Book selectBook(String bookId);
+	
+	// 도서 등록
+	public int insertBook(Book book);
+	
+	// 도서 수정
+	public int updateBook(Book book);
+	
+	// 도서 삭제
+	public int deleteBook(String bookId);
 }
--- src/main/java/com/erl/pageflow/book/model/vo/Book.java
+++ src/main/java/com/erl/pageflow/book/model/vo/Book.java
@@ -1,8 +1,161 @@
 package com.erl.pageflow.book.model.vo;
 
 import java.io.Serializable;
+import java.sql.Date;
 
 public class Book implements Serializable {
 	private static final long serialVersionUID = -6417677392956824886L;
+	
+	private int bookId;
+	private int writerId;
+	private String bookName;
+	private String writerName;
+	private int bookPriceS;
+	private int bookPriceR;
+	private int bookPrice;
+	private String category;
+	private String isbn;
+	private String bookState;
+	private String engraving;
+	private Date pubDate;
+	private Date regDate;
+	
+	public Book() {
+		super();
+	}
 
+	public Book(int bookId, int writerId, String bookName, String writerName, int bookPriceS, int bookPriceR,
+			int bookPrice, String category, String isbn, String bookState, String engraving, Date pubDate,
+			Date regDate) {
+		super();
+		this.bookId = bookId;
+		this.writerId = writerId;
+		this.bookName = bookName;
+		this.writerName = writerName;
+		this.bookPriceS = bookPriceS;
+		this.bookPriceR = bookPriceR;
+		this.bookPrice = bookPrice;
+		this.category = category;
+		this.isbn = isbn;
+		this.bookState = bookState;
+		this.engraving = engraving;
+		this.pubDate = pubDate;
+		this.regDate = regDate;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public int getWriterId() {
+		return writerId;
+	}
+
+	public void setWriterId(int writerId) {
+		this.writerId = writerId;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getWriterName() {
+		return writerName;
+	}
+
+	public void setWriterName(String writerName) {
+		this.writerName = writerName;
+	}
+
+	public int getBookPriceS() {
+		return bookPriceS;
+	}
+
+	public void setBookPriceS(int bookPriceS) {
+		this.bookPriceS = bookPriceS;
+	}
+
+	public int getBookPriceR() {
+		return bookPriceR;
+	}
+
+	public void setBookPriceR(int bookPriceR) {
+		this.bookPriceR = bookPriceR;
+	}
+
+	public int getBookPrice() {
+		return bookPrice;
+	}
+
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getBookState() {
+		return bookState;
+	}
+
+	public void setBookState(String bookState) {
+		this.bookState = bookState;
+	}
+
+	public String getEngraving() {
+		return engraving;
+	}
+
+	public void setEngraving(String engraving) {
+		this.engraving = engraving;
+	}
+
+	public Date getPubDate() {
+		return pubDate;
+	}
+
+	public void setPubDate(Date pubDate) {
+		this.pubDate = pubDate;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [bookId=" + bookId + ", writerId=" + writerId + ", bookName=" + bookName + ", writerName="
+				+ writerName + ", bookPriceS=" + bookPriceS + ", bookPriceR=" + bookPriceR + ", bookPrice=" + bookPrice
+				+ ", category=" + category + ", isbn=" + isbn + ", bookState=" + bookState + ", engraving=" + engraving
+				+ ", pubDate=" + pubDate + ", regDate=" + regDate + "]";
+	}
 }
