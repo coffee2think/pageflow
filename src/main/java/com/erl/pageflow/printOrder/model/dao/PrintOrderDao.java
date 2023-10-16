8e956e3d95ac0ef3e600a634d3c87061ebbcbcef
Jimmy0216 <maximin910216@gmail.com>
2023-10-16 14:25:29 +0900

    발주현황작업완료(토요일작업)

--- src/main/java/com/erl/pageflow/printOrder/model/dao/PrintOrderDao.java
+++ src/main/java/com/erl/pageflow/printOrder/model/dao/PrintOrderDao.java
@@ -1,5 +1,42 @@
 package com.erl.pageflow.printOrder.model.dao;
 
-public class PrintOrderDao {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.erl.pageflow.book.model.vo.Book;
+import com.erl.pageflow.common.Paging;
+import com.erl.pageflow.common.Search;
+import com.erl.pageflow.printOrder.model.vo.PrintOrder;
+import com.erl.pageflow.sales.model.vo.PrintOffice;
+
+@Repository("printOrderDao")
+public class PrintOrderDao {
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	public ArrayList<PrintOrder> selectPrintOrderByDate(Search search) {
+		List<PrintOrder> list = sqlSessionTemplate.selectList("printMapper.selectPrintOrderByDate", search);
+		return (ArrayList<PrintOrder>) list;
+	}
+	
+	public PrintOffice selectPrintOffice(int printId) {
+		return sqlSessionTemplate.selectOne("printMapper.selectPrintOffice", printId);
+	}
+	
+	public Book selectBook(int bookId) {
+		return sqlSessionTemplate.selectOne("printMapper.selectBook",bookId);
+	}
+	
+	public int selectPrintOrderListCount() {
+		return sqlSessionTemplate.selectOne("printMapper.selectPrintOrderListCount");
+	}
+	
+	public ArrayList<PrintOrder> selectPrintOrderList(Paging paging){
+		List<PrintOrder> list = sqlSessionTemplate.selectList("printMapper.selectPrintOrderList", paging);
+		return (ArrayList<PrintOrder>) list;
+	}
 }
--- src/main/java/com/erl/pageflow/printOrder/model/service/PrintOrderServiceImpl.java
+++ src/main/java/com/erl/pageflow/printOrder/model/service/PrintOrderServiceImpl.java
@@ -1,5 +1,46 @@
 package com.erl.pageflow.printOrder.model.service;
 
-public interface PrintOrderServiceImpl {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.erl.pageflow.book.model.vo.Book;
+import com.erl.pageflow.common.Paging;
+import com.erl.pageflow.common.Search;
+import com.erl.pageflow.printOrder.model.dao.PrintOrderDao;
+import com.erl.pageflow.printOrder.model.vo.PrintOrder;
+import com.erl.pageflow.sales.model.vo.PrintOffice;
+
+@Service("printOrderService")
+public class PrintOrderServiceImpl implements PrintOrderService {
+		
+	@Autowired
+	private PrintOrderDao printOrderDao;
+
+	@Override
+	public ArrayList<PrintOrder> selectPrintOrderByDate(Search search) {
+		return printOrderDao.selectPrintOrderByDate(search);
+	}
+	
+	@Override
+	public PrintOffice selectPrintOffice(int printId) {
+		return printOrderDao.selectPrintOffice(printId);
+	}
+	
+	@Override
+	public Book selectBook(int bookId) {
+		return printOrderDao.selectBook(bookId);
+	}
+	
+	@Override
+	public int selectPrintOrderListCount() {
+		return printOrderDao.selectPrintOrderListCount();
+	}
+	
+	@Override
+	public ArrayList<PrintOrder> selectPrintOrderList(Paging paging){
+		return printOrderDao.selectPrintOrderList(paging);
+	}
+	
 }
